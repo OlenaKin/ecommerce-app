40e8b65eab4d96078c673269a1d8bc533c800df7
Olena Onyshkiv <elena.onishkiv@gmail.com>
2026-08-14 20:09:36 +0200

    build: skip type-checking, successful build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/ecommerce-app/',
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
