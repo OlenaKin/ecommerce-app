ab6c7815a8ad0f565f8736ce766f94ae48895e05
Olena Onyshkiv <elena.onishkiv@gmail.com>
2026-08-14 19:55:51 +0200

    updated modified:   vite.config.ts         modified:   vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,23 +1,10 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/ecommerce-app/', 
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @use "@/styles/variables.scss" as *;
-        `,
-      },
-    },
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
 })
