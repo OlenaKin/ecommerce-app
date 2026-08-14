f30d8e67f5245ac8fe804a9910636b7ac65d543c
Olena Onyshkiv <elena.onishkiv@gmail.com>
2026-08-14 22:08:47 +0200

    modified:   package-lock.json         modified:   package.json         modified:   vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,31 @@
-import { defineConfig } from 'vitest/config'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import envCompatible from 'vite-plugin-env-compatible' // 👈 ADD THIS
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
+  base: '/ecommerce-app/',
+  plugins: [
+    vue(),
+    vueDevTools(),
+    envCompatible({
+      prefix: 'VUE_APP_', // 👈 Keeps your VUE_APP_ variables alive
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/variables.scss" as *;
+        `,
+      },
+    },
   },
 })
