ed6f2d0a650636444c38896630e1b45af96eb763
Olena Onyshkiv <elena.onishkiv@gmail.com>
2026-08-14 17:53:19 +0200

    modifies vitest.config.js

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 export default defineConfig({
+    base: '/ecommerce-app/',
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
